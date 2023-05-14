cb80f73bd7dcd66be86debde2a5a31f3b6a6c386
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-05-14 21:34:47 +0700

    add color variant

--- app/src/main/java/com/veroanggra/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/veroanggra/calculatorapp/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.primary
                 ) {
                     ArithmaticCalculateScreen(modifier = Modifier, calculateViewModel = calculateViewModel,)
                 }
--- app/src/main/java/com/veroanggra/calculatorapp/ui/theme/Color.kt
+++ app/src/main/java/com/veroanggra/calculatorapp/ui/theme/Color.kt
@@ -2,16 +2,21 @@ package com.veroanggra.calculatorapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
+// Light
+val lightBackground = Color(0xFFF9F9F9)
 val lightYellow = Color(0xFFF4BC4E)
 val lightTransparentYellow = Color(0xFFF5F1E5)
 val lightTransparentPurple = Color(0xFFDFD5E7)
-val lightFontYellow = Color(0xFFC5B489)
 val lightPurple = Color(0xFF832FD6)
+val lightFontYellow = Color(0xFFC5B489)
+val lightFontPurple = Color(0xFF4F287A)
+
+
+// Dark
+val darkBackground = Color(0xFF3D4041)
+val darkYellow = Color(0xFFF2BB4D)
+val darkTransparentYellow = Color(0xFF4A443C)
+val darkPurple = Color(0XFF8D34E7)
+val darkTransparentPurple = Color(0xFF48375D)
+val darkFontYellow = Color(0xFF816D50)
+val darkFontPurple = Color(0xFFBD89E1)
