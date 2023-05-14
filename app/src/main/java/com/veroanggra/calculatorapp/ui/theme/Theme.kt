09a9a86e104756e805678a53cf85d53337dbde2f
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-05-14 21:34:47 +0700

    add color variant

--- app/src/main/java/com/veroanggra/calculatorapp/ui/theme/Theme.kt
+++ app/src/main/java/com/veroanggra/calculatorapp/ui/theme/Theme.kt
@@ -16,32 +16,30 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = darkBackground,
+    secondary = darkYellow,
+    tertiary = darkTransparentYellow,
+    background = darkPurple,
+    surface = darkTransparentPurple,
+    onPrimary = darkFontYellow,
+    onSecondary = darkFontPurple
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = lightBackground,
+    secondary = lightYellow,
+    tertiary = lightTransparentYellow,
+    background = lightPurple,
+    surface = darkTransparentPurple,
+    onPrimary = lightFontYellow,
+    onSecondary = lightFontPurple
 )
 
 @Composable
 fun CalculatorAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -58,7 +56,8 @@ fun CalculatorAppTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
         }
     }
 
